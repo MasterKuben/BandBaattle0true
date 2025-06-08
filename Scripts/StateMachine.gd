6add93ea5afd1701fb712b5d173ca02f6c57098c
MasterKuben <kubenstudio@gmail.com>
2025-06-08 16:47:08 +0100

    Added more to states
    Also reorganized a few files

--- Scripts/StateMachine.gd
+++ Scripts/StateMachine.gd
@@ -7,3 +7,26 @@ var walk_state = false
 var run_state = false
 var back_walk_state = false
 var down_back_state = false
+#Jump states
+var forward_jump = false
+var back_jump =  false
+var block_jump =  false
+var neutral_jump = false
+#attack states
+var high_p = false
+var mid_p = false
+var low_p = false
+
+var high_k = false
+var mid_k = false
+var low_k = false
+
+var high_i = false
+var mid_i = false
+var low_i = false
+
+var high_s = false
+var mid_s = false
+var low_s = false
+
+var special_attack = false
---
+++ Scripts/bass_fighter_states.gd
@@ -0,0 +1,3 @@
+extends Node
+var fighterMachine : uniStateMachine
+var current_state = fightMachine.
